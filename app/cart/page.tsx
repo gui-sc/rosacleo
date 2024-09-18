8b352b3fe2fc69df0ffdb7927efc5e7dc4d73021
guilherme coelho <guilhermeifc7@gmail.com>
2024-09-17 21:02:44 -0300

    feat: add address info and fix button logout

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link'; // Para navegação
 import Header from '../components/header';
 import { useCart } from '../contexts/cartContext';
@@ -9,14 +9,24 @@ export default function CartPage() {
     const { cartItems } = useCart();
     const [activeTab, setActiveTab] = useState('details'); // Estado para alternar entre as abas
     const [isModalOpen, setIsModalOpen] = useState(false); // Estado para abrir/fechar o modal
-
+    const [cep, setCep] = useState(''); // Estado para armazenar o CEP
+    const [total, setTotal] = useState(0); // Estado para armazenar o total do carrinho
     // Função para calcular o total do carrinho
-    const cartTotal = Object.values(cartItems).reduce((acc, item) => {
+    const cartTotal = Number(Object.values(cartItems).reduce((acc, item) => {
         return acc + item.item.price * item.quantity;
-    }, 0);
+    }, 0).toFixed(2));
     const totalItems = Object.values(cartItems).reduce((acc, item) => {
         return acc + item.quantity;
     }, 0);
+
+    useEffect(() => {
+        setTotal(cartTotal);
+    }, [cartItems]);
+
+    useEffect(() => {
+        if (cep.length === 8) setTotal(Number((total + 20).toFixed(2)));
+    }, [cep]);
+
 
     return (
         <div className="min-h-screen flex flex-col bg-gray-100">
@@ -41,13 +51,19 @@ export default function CartPage() {
                                 onClick={() => setActiveTab('details')}
                                 className={`px-4 py-2 ${activeTab === 'details' ? 'border-b-2 border-[--primary] text-[--primary]' : ''}`}
                             >
-                                Detalhes do Produto
+                                Itens do Carrinho
                             </button>
                             <button
                                 onClick={() => setActiveTab('shipping')}
                                 className={`px-4 py-2 ${activeTab === 'shipping' ? 'border-b-2 border-[--primary] text-[--primary]' : ''}`}
                             >
                                 Informações de Frete
+                            </button>
+                            <button
+                                onClick={() => setActiveTab('address')}
+                                className={`px-4 py-2 ${activeTab === 'address' ? 'border-b-2 border-[--primary] text-[--primary]' : ''}`}
+                            >
+                                Informações de Endereço
                             </button>
                         </div>
 
@@ -66,18 +82,91 @@ export default function CartPage() {
                                 {/* Total do carrinho */}
                                 <div className="mt-4 flex justify-between items-center font-bold text-xl">
                                     <span>Total:</span>
-                                    <span>R$ {cartTotal}</span>
+                                    <span>R$ {total}</span>
                                 </div>
                             </div>
-                        ) : (
+                        ) : (activeTab === 'shipping' ? (
                             <div>
                                 {/* Conteúdo da aba de informações de frete */}
                                 <h3 className="text-lg font-bold">Opções de Frete</h3>
                                 <p>Informe seu CEP para calcular o frete:</p>
-                                <input type="text" placeholder="Digite seu CEP" className="mt-2 p-2 border rounded w-full" />
-                                {/* Outras informações e opções de frete */}
+                                <input type="text" placeholder="Digite seu CEP"
+                                    value={cep}
+                                    onChange={(e) => setCep((prev) => {
+                                        if (prev.length === 8 && e.target.value.length < 8) {
+                                            setTotal(total - 20);
+                                        }
+                                        return e.target.value;
+                                    })}
+                                    className="mt-2 p-2 border rounded w-full" />
+                                {cep.length == 8 && (
+                                    <div className="mt-4">
+                                        {/* Opções de frete */}
+                                        <div className="flex items-center justify-between border-b pb-2">
+                                            <span>Entrega Expressa</span>
+                                            <span>R$ 20,00</span>
+                                            <span className="text-xs">Entrega em até 2 dias úteis</span>
+                                            <button
+                                                onClick={() => setTotal(total + 20)}
+                                                className="px-4 py-2 bg-[--primary] text-white rounded-md hover:bg-[--background] hover:text-[--primary] border-2 border-[--background] hover:border-[--primary] transition-all duration-300 ease-in-out"
+                                            >
+                                                Selecionar
+                                            </button>
+                                        </div>
+                                        <div className="flex items-center justify-between border-b pb-2">
+                                            <span>Entrega Normal</span>
+                                            <span>R$ 10,00</span>
+                                            <span className="text-xs">Entrega em até 5 dias úteis</span>
+                                            <button
+                                                onClick={() => setTotal(total + 10)}
+                                                className="px-4 py-2 bg-[--primary] text-white rounded-md hover:bg-[--background] hover:text-[--primary] border-2 border-[--background] hover:border-[--primary] transition-all duration-300 ease-in-out"
+                                            >
+                                                Selecionar
+                                            </button>
+                                        </div>
+                                    </div>
+                                )}
+                                {/* Total do carrinho */}
+                                <div className="mt-4 flex justify-between items-center font-bold text-xl">
+                                    <span>Total:</span>
+                                    <span>R$ {total}</span>
+                                </div>
                             </div>
-                        )}
+                        ) : (
+                            <div>
+                                {/* Conteúdo da aba de informações de endereço */}
+                                <h3 className="text-lg font-bold">Informações de Endereço</h3>
+                                <div className="mt-4">
+                                    <label className="block mb-2">Nome</label>
+                                    <input type="text" className="w-full p-2 border rounded" />
+                                </div>
+                                <div className="mt-4">
+                                    <label className="block mb-2">Endereço</label>
+                                    <input type="text" className="w-full p-2 border rounded" />
+                                </div>
+                                <div className="mt-4">
+                                    <label className="block mb-2">Número</label>
+                                    <input type="text" className="w-full p-2 border rounded" />
+                                </div>
+                                <div className="mt-4">
+                                    <label className="block mb-2">Complemento</label>
+                                    <input type="text" className="w-full p-2 border rounded" />
+                                </div>
+                                <div className="mt-4">
+                                    <label className="block mb-2">CEP</label>
+                                    <input type="text" className="w-full p-2 border rounded" />
+                                </div>
+                                <div className="mt-4">
+                                    <label className="block mb-2">Cidade</label>
+                                    <input type="text" className="w-full p-2 border rounded" />
+                                </div>
+                                <div className="mt-4">
+                                    <label className="block mb-2">Estado</label>
+                                    <input type="text" className="w-full p-2 border rounded" />
+                                </div>
+                            </div>
+                        ))}
+
 
                         {/* Botão para finalizar a compra */}
                         <button
--- app/components/sidebarCart.tsx
+++ app/components/sidebarCart.tsx
@@ -24,73 +24,71 @@ const ShoppingCartSidebar = () => {
 
     return (
         <div>
-    {/* Botão para abrir o carrinho */}
-    <button
-        className="text-2xl md:text-3xl"
-        onClick={() => setIsCartOpen(true)}
-    >
-        <div className="relative w-8 md:w-10">
-            <LocalMallIcon className="text-[--primary]" />
-            {totalItems > 0 && (
-                <span
-                    className="absolute top-0 right-0 bg-[--background] text-[--primary] border-[--primary] border-2 text-xs rounded-full w-4 h-4 flex items-center justify-center"
-                >
-                    {totalItems}
-                </span>
-            )}
-        </div>
-    </button>
-
-    {/* Sidebar do carrinho */}
-    <div
-        className={`fixed top-0 right-0 h-full w-full max-w-xs md:w-80 bg-white shadow-md p-4 md:p-5 transform ${isCartOpen ? 'translate-x-0' : 'translate-x-full'
-            } transition-transform duration-300 ease-in-out z-50`}
-    >
-        {/* Botão para fechar o carrinho */}
-        <button
-            className="text-2xl mb-4 md:text-3xl"
-            onClick={() => setIsCartOpen(false)}
-        >
-            <HiX />
-        </button>
-
-        <h2 className="text-lg font-bold mb-4 md:text-xl">
-            Carrinho de Compras
-        </h2>
-
-        {/* Área de conteúdo com rolagem */}
-        <div className="h-[calc(100vh-150px)] overflow-y-auto pr-2">
-            {/* Lista de itens do carrinho */}
-            <div className="flex flex-col gap-4">
-                {Object.keys(cartItems).map((key) => (
-                    <div key={key} className="flex justify-between items-center border-b pb-2 text-sm md:text-base">
-                        <span>{cartItems[key].item.name}</span>
-                        <div>
-                            <span className="text-xs font-semibold">{cartItems[key].quantity}x{" "}</span>
-                            <span>R$ {cartItems[key].item.price}</span>
-                        </div>
-                    </div>
-                ))}
-            </div>
-
-            {/* Total do carrinho */}
-            <div className="mt-4 flex justify-between items-center font-bold text-sm md:text-base">
-                <span>Total:</span>
-                <span>R$ {cartTotal}</span>
-            </div>
-
-            {/* Botão para finalizar a compra */}
+            {/* Botão para abrir o carrinho */}
             <button
-                className={`mt-4 w-full px-3 py-2 bg-[--primary] text-white rounded-md 
-                hover:bg-[--background] hover:text-[--primary] border-2 border-[--background] 
-                hover:border-[--primary] transition-all duration-300 ease-in-out md:text-base`}
-                onClick={() => { handleCheckout() }}
+                className="text-2xl md:text-3xl"
+                onClick={() => setIsCartOpen(true)}
             >
-                Finalizar Compra
+                <div className="relative w-8 md:w-10">
+                    <LocalMallIcon className="text-[--primary]" />
+                    {totalItems > 0 && (
+                        <span
+                            className="absolute top-0 right-0 bg-[--background] text-[--primary] border-[--primary] border-2 text-xs rounded-full w-4 h-4 flex items-center justify-center"
+                        >
+                            {totalItems}
+                        </span>
+                    )}
+                </div>
             </button>
+
+            {/* Sidebar do carrinho */}
+            <div
+                className={`fixed top-0 right-0 h-full w-full max-w-xs md:w-80 bg-white shadow-md p-4 md:p-5 transform ${isCartOpen ? 'translate-x-0' : 'translate-x-full'
+                    } transition-transform duration-300 ease-in-out z-50`}
+            >
+                {/* Botão para fechar o carrinho */}
+                <button
+                    className="text-2xl mb-4 md:text-3xl"
+                    onClick={() => setIsCartOpen(false)}
+                >
+                    <HiX />
+                </button>
+
+                <h2 className="text-lg font-bold mb-4 md:text-xl">
+                    Carrinho de Compras
+                </h2>
+
+                {/* Área de conteúdo com rolagem */}
+                <div className="h-[calc(100vh-150px)] overflow-y-auto pr-2">
+                    {/* Lista de itens do carrinho */}
+                    <div className="flex flex-col gap-4">
+                        {Object.keys(cartItems).map((key) => (
+                            <div key={key} className="flex justify-between items-center border-b pb-2 text-sm md:text-base">
+                                <span>{cartItems[key].item.name}</span>
+                                <div>
+                                    <span className="text-xs font-semibold">{cartItems[key].quantity}x{" "}</span>
+                                    <span>R$ {cartItems[key].item.price}</span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    {/* Total do carrinho */}
+                    <div className="mt-4 flex justify-between items-center font-bold text-sm md:text-base">
+                        <span>Total:</span>
+                        <span>R$ {cartTotal}</span>
+                    </div>
+
+                    {/* Botão para finalizar a compra */}
+                    <button
+                        className="w-full px-3 py-1 mt-4 bg-[--primary] text-white text-sm md:text-base rounded-md hover:bg-white hover:text-[--primary] border-2 hover:border-[--primary] border-white transition-all duration-300 ease-in-out"
+                        onClick={handleCheckout}
+                    >
+                        Finalizar Compra
+                    </button>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
 
     );
 };
